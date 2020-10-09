d6bb285feb70d6269f27f087290f0e30bb6b5ec4
Adrian Anaya <aanaya79@cnm.edu>
2020-10-09 09:40:25 -0600

    Started Javadoc comments.

--- src/edu/cnm/deepdive/codebreaker/controller/Codebreaker.java
+++ src/edu/cnm/deepdive/codebreaker/controller/Codebreaker.java
@@ -8,11 +8,26 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.security.SecureRandom;
 
+/**
+ * Implements a simple console-mode game in which the program generates a secret code and the user
+ * guesses the code. After each guess, the program displays the results of that guess--namely, the
+ * number of characters in the correct position, and the number of additional characters that are
+ * in the secret code and the guess, but are not in the correct position in the guess.
+ *
+ * @version 1.0
+ * @author Adrian Anaya
+ */
 public class Codebreaker {
 
   private static final String CHARACTER_POOL = "ROYGBIV";
   private static final int CODE_LENGTH = 4;
 
+  /**
+   * Entry point for codebreaker game. Creates an instance {@link Game}, and repeatedly takes
+   * user input to submit as a guess, using the {@link Game#guess(String)} method.
+   *
+   * @param args Command-line arguments (ignored)
+   */
   public static void main(String[] args) {
     Game game = new Game(CHARACTER_POOL, CODE_LENGTH, new SecureRandom());
     System.out.printf("Pool: %s. Code Length: %d%n", CHARACTER_POOL, CODE_LENGTH);
--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -9,10 +9,20 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * Generates a secret code.
+ */
 public class Code {
 
   private final char[] secret;
 
+  /**
+   * Creates a random code from given string of characters.
+   *
+   * @param pool The characters allowed to be used in the code.
+   * @param length The length of the code.
+   * @param rng Generates a random string of letters from given string.
+   */
   public Code(String pool, int length, Random rng) {
     secret = new char[length];
     for (int i = 0; i < length; i++) {
@@ -25,6 +35,11 @@ public class Code {
     return new String(secret);
   }
 
+  /**
+   * Uses the secret code constructed in the {@link Code} and compares the users guess to the
+   * actual code. The program will respond with how many colors they got correct and how many were
+   * close, but not in the right position.
+   */
   public class Guess {
 
     private static final String STRING_FORMAT = "{text: \"%s\", correct: %d, close: %d}";
@@ -32,6 +47,11 @@ public class Code {
     private final int correct;
     private final int close;
 
+    /**
+     * Tests how many of the characters in the guess were correct, and how many were close.
+     *
+     * @param text Takes the users guess text
+     */
     public Guess(String text) {
       this.text = text;
       int correct = 0;
@@ -83,14 +103,23 @@ public class Code {
       return String.format(STRING_FORMAT, text, correct, close);
     }
 
+    /**
+     * Returns text of this instance.
+     */
     public String getText() {
       return text;
     }
 
+    /**
+     * Returns the number of correct colors in the secret code that also have the same position.
+     */
     public int getCorrect() {
       return correct;
     }
 
+    /**
+     * Returns the number of colors that are correct, but not in the correct position.
+     */
     public int getClose() {
       return close;
     }
